995eca7f9a8659bd342826f3eef84f924de799eb
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:57:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    array.pop!
+    array.pop
       for element in array
         word << "#{element}, "
       end
