f9228901dd9281bd61e942d468bf1230e9db0fdd
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:47:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
 def oxford_comma(array)
+  counter = array.size
+  word = String.new
   if array.size == 2
     array.join(" and ")
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
-
   else
-  array.join(" , ")
+    while counter<0
+      word << "#{element}, "
+      counter -=1
+    end
+    word << "and #{array[array.size]}"
+    word
   end
 end
