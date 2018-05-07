b78c47138afffeb1783a82d15164a076d564deaf
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:57:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,10 +8,10 @@ def oxford_comma(array)
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    new_array = array.delete_at(-1)
-    for element in new_array
-      word << "#{element}, "
-    end
+    array.pop!
+      for element in array
+        word << "#{element}, "
+      end
     word << "and #{array[-1]}"
     return word
   end
