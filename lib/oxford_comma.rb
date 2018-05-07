f18c3e8dc11497fce23f00104c85839b14ea1325
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:43:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    "#{array[0]}, #{array[1]} and #{array[2]}"
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
 
   else
   array.join(" , ")
