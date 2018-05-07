97223dd2b67d3aa1f62c771e81e911f542d878a7
Tony Ho <ho.tonym@gmail.com>
2018-05-07 08:48:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   counter = array.size
   word = String.new
-  if array.size == 2
+  if array.size == 1
+    array
+  elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
